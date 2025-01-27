335fb2e892f2cab14b205fee7a85469a3627dede
Talelkarif1 <Talelkarif1186@gmail.com>
2025-01-27 18:45:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
+
 df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
+print(df.columns)
 
 features = ['PPE', 'DFA']
 target = 'status'
 x = df[features]
 y = df[target]
 
-from sklearn.preprocessing import MimMaxScaler 
+from sklearn.preprocessing import MinMaxScaler
+
 scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 
@@ -15,10 +18,16 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42
 
 from sklearn.svm import SVC
+
 model = SVC()
 model.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
+
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)                           
+print(f'Accuracy: {accuracy}')
+
+import joblib
+
+joblib.dump(model, 'tal_model.joblib')
